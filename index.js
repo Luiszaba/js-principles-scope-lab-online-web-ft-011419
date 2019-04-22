8ed7ff5351211893ab39db2783a6aa7a06d7436a
Luis Zabala <luis.zabala076@gmail.com>
2019-04-22 00:01:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 // Write your solution in this file!
 
 var customerName = "bob";
+var bestCustomer  = '';
 
 function upperCaseCustomerName() {
   customerName = customerName.toUpperCase();
   return customerName;
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+  return bestCustomer;
 }
